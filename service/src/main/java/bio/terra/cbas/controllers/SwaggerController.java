67f0d61a733a80f97e68d89b1a3ce5739b76b014
Chris Llanwarne <cjllanwarne@gmail.com>
2022-04-08 11:33:08 -0400

    Simplified swagger

--- service/src/main/java/bio/terra/cbas/controllers/SwaggerController.java
+++ service/src/main/java/bio/terra/cbas/controllers/SwaggerController.java
@@ -1,42 +1,17 @@
-// package bio.terra.cbas.controllers;
-//
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.nio.charset.StandardCharsets;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.core.io.ClassPathResource;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-//
-// @Controller
-// public class SwaggerController {
-//  private static final Logger logger = LoggerFactory.getLogger(SwaggerController.class);
-//
-//  private String swaggerClientId;
-//
-//  public SwaggerController() {
-//    try {
-//      try (var reader =
-//          new BufferedReader(
-//              new InputStreamReader(
-//                  new ClassPathResource("rendered/swagger-client-id").getInputStream(),
-//                  StandardCharsets.UTF_8))) {
-//        swaggerClientId = reader.readLine();
-//      }
-//    } catch (IOException e) {
-//      logger.error(
-//          "It doesn't look like configs have been rendered! Unable to parse swagger client id.",
-// e);
-//      swaggerClientId = "";
-//    }
-//  }
-//
-//  @GetMapping({"/", "/index.html", "swagger-ui.html"})
-//  public String getSwagger(Model model) {
-//    model.addAttribute("clientId", swaggerClientId);
-//    return "swagger-ui";
-//  }
-// }
+package bio.terra.cbas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/")
+public class SwaggerController {
+
+  @GetMapping({"/", "swagger-ui"})
+  public RedirectView getSwagger(RedirectAttributes redirectAttributes) {
+    return new RedirectView("swagger-ui.html");
+  }
+}
