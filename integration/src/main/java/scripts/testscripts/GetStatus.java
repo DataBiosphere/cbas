a0316deb62600a36d54a101d270d4a4960b0d45d
Adam Nichols <aednichols@gmail.com>
2022-05-16 18:32:25 -0400

    Unbreak?

--- integration/src/main/java/scripts/testscripts/GetStatus.java
+++ integration/src/main/java/scripts/testscripts/GetStatus.java
@@ -3,7 +3,7 @@ package scripts.testscripts;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import bio.terra.catalog.api.PublicApi;
+//import bio.terra.catalog.api.PublicApi;
 import bio.terra.testrunner.runner.TestScript;
 import bio.terra.testrunner.runner.config.TestUserSpecification;
 import com.google.api.client.http.HttpStatusCodes;
