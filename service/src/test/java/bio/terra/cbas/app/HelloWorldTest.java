b5d49e9d51f809f126a7c7716245afef22d11f42
Chris Llanwarne <cjllanwarne@gmail.com>
2022-05-24 15:11:33 -0400

    Addition of a HelloWorld unit test

---
+++ service/src/test/java/bio/terra/cbas/app/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package bio.terra.cbas.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/** Placeholder test case to prove the github action integration. */
+class HelloWorldTest {
+  @Test
+  void addition() {
+    assertEquals(2, 1 + 1);
+  }
+}
