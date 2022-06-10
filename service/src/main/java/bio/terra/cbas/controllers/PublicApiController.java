8a1bc33f870667c1a94a4958a5ed3f880d7a91fb
Adam Nichols <aednichols@gmail.com>
2022-06-10 18:36:59 -0400

    BW-1250 Basic status check against Cromwell URL (#18)

--- service/src/main/java/bio/terra/cbas/controllers/PublicApiController.java
+++ service/src/main/java/bio/terra/cbas/controllers/PublicApiController.java
@@ -1,7 +1,13 @@
 package bio.terra.cbas.controllers;
 
 import bio.terra.cbas.api.PublicApi;
+import bio.terra.cbas.config.CromwellServerConfiguration;
 import bio.terra.cbas.model.SystemStatus;
+import bio.terra.cbas.model.SystemStatusSystems;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,8 +15,35 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PublicApiController implements PublicApi {
 
+  private final CromwellServerConfiguration cromwellConfig;
+
+  @Autowired
+  public PublicApiController(CromwellServerConfiguration cromwellConfig) {
+    this.cromwellConfig = cromwellConfig;
+  }
+
   @Override
   public ResponseEntity<SystemStatus> getStatus() {
-    return new ResponseEntity<>(new SystemStatus().ok(true), HttpStatus.OK);
+
+    String result;
+    boolean isOk;
+    try {
+      URL url = new URL(this.cromwellConfig.healthUri());
+      URLConnection connection = url.openConnection();
+      connection.setConnectTimeout(5000);
+      var stream = connection.getInputStream();
+
+      result = new String(stream.readAllBytes(), StandardCharsets.UTF_8);
+      isOk = true;
+    } catch (Exception e) {
+      result = e.getLocalizedMessage();
+      isOk = false;
+    }
+
+    return new ResponseEntity<>(
+        new SystemStatus()
+            .ok(true)
+            .putSystemsItem("Cromwell", new SystemStatusSystems().ok(isOk).addMessagesItem(result)),
+        HttpStatus.OK);
   }
 }
