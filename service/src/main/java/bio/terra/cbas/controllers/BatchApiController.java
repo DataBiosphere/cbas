6de3359ddccff51b943a282d3832771873bbe43f
Adam Nichols <aednichols@gmail.com>
2022-06-24 13:54:40 -0400

    Progress

--- service/src/main/java/bio/terra/cbas/controllers/BatchApiController.java
+++ service/src/main/java/bio/terra/cbas/controllers/BatchApiController.java
@@ -1,7 +1,6 @@
 package bio.terra.cbas.controllers;
 
 import bio.terra.cbas.api.BatchApi;
-import cromwell.client.ApiClient;
 import java.util.Collections;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +12,6 @@ public class BatchApiController implements BatchApi {
   @Override
   public ResponseEntity<List<String>> listMethods() {
     // TODO: Get an answer from the database?
-
-    ApiClient client = new ApiClient();
-    System.out.println("HACK: Appease spotbugs by printing client: " + client);
-
     return ResponseEntity.ok(
         Collections.singletonList(
             "https://raw.githubusercontent.com/broadinstitute/cromwell/develop/centaur/src/main/resources/standardTestCases/hello/hello.wdl"));
