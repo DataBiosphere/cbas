200f28a643e81791491587c5df4cbbe5532bccc3
Adam Nichols <aednichols@gmail.com>
2022-06-30 11:22:36 -0400

    BW-1279 Add Cromwell client library to CBAS, set up workflow submission (#21)

--- service/src/main/java/bio/terra/cbas/controllers/RunsApiController.java
+++ service/src/main/java/bio/terra/cbas/controllers/RunsApiController.java
@@ -1,9 +1,13 @@
 package bio.terra.cbas.controllers;
 
 import bio.terra.cbas.api.RunsApi;
+import bio.terra.cbas.config.CromwellServerConfiguration;
 import bio.terra.cbas.model.RunState;
 import bio.terra.cbas.model.RunStateResponse;
+import cromwell.client.ApiClient;
+import cromwell.client.api.WorkflowsApi;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,10 +15,53 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class RunsApiController implements RunsApi {
 
+  private final CromwellServerConfiguration cromwellConfig;
+
+  @Autowired
+  public RunsApiController(CromwellServerConfiguration cromwellConfig) {
+    this.cromwellConfig = cromwellConfig;
+  }
+
   @Override
   public ResponseEntity<RunStateResponse> postRun(String workflowUrl, Object workflowParams) {
+
+    ApiClient client = new ApiClient();
+    client.setBasePath(this.cromwellConfig.baseUri());
+    WorkflowsApi workflowsApi = new WorkflowsApi(client);
     String runId = UUID.randomUUID().toString();
-    return new ResponseEntity<>(
-        new RunStateResponse().runId(runId).state(RunState.QUEUED), HttpStatus.CREATED);
+
+    ResponseEntity result;
+
+    try {
+      workflowsApi.submit(
+          "v1",
+          null,
+          workflowUrl,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null);
+
+      result =
+          new ResponseEntity<>(
+              new RunStateResponse().runId(runId).state(RunState.QUEUED), HttpStatus.CREATED);
+    } catch (cromwell.client.ApiException e) {
+      System.out.println(e);
+      result =
+          new ResponseEntity<>(
+              new RunStateResponse().runId(runId).state(RunState.SYSTEM_ERROR),
+              HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return result;
   }
 }
