cd742a883667c981fbb97f5b3529058ec3e0612d
Katrina Pierre <kpierre@broadinstitute.org>
2022-07-13 22:23:34 -0400

    Mock RunLogResponse functioning

--- service/src/main/java/bio/terra/cbas/controllers/RunsApiController.java
+++ service/src/main/java/bio/terra/cbas/controllers/RunsApiController.java
@@ -2,11 +2,15 @@ package bio.terra.cbas.controllers;
 
 import bio.terra.cbas.api.RunsApi;
 import bio.terra.cbas.config.CromwellServerConfiguration;
+import bio.terra.cbas.model.LogRunRequest;
 import bio.terra.cbas.model.RunLogResponse;
+import bio.terra.cbas.model.RunRequest;
 import bio.terra.cbas.model.RunState;
 import bio.terra.cbas.model.RunStateResponse;
 import cromwell.client.ApiClient;
 import cromwell.client.api.WorkflowsApi;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,13 +29,41 @@ public class RunsApiController implements RunsApi {
 
   // add new endpoint here @@Override
   // add details about updating swagger to development.md
+
+  //  @Override
+  //  public ResponseEntity<RunStateResponse> postRun(String workflowUrl, Object workflowParams) {
+  //    String runId = UUID.randomUUID().toString();
+  //    return new ResponseEntity<>(
+  //        new RunStateResponse().runId(runId).state(RunState.QUEUED), HttpStatus.CREATED);
+
+
+  //workflow ID -- runid
+  //workflow name -- name(task_logs)
+  //workflow source -- workflow_url(request)
+  //status -- state
+  //inputs -- workflow_params(request)
+  //submission date -- start & end (task_logs)
+
   @Override
-  public ResponseEntity<RunLogResponse> getRun(RunLogResponse body){
-    return new ResponseEntity<>(new RunLogResponse(), HttpStatus.OK);
+  public ResponseEntity<RunLogResponse> getRun() {
+    List<LogRunRequest> runs = new ArrayList<>();
+    String runId = UUID.randomUUID().toString();
+    String name = "BAS";
+
+    runs.add((new LogRunRequest()
+            .runId(runId)
+            .state(RunState.UNKNOWN)
+            .workflowUrl("urlHere")
+            .name(name)
+            .workflowParams("params")
+            .submissionDate("today")
+        ));
+
+    return ResponseEntity.ok(new RunLogResponse().runs(runs));
   }
 
   @Override
-  public ResponseEntity<RunStateResponse> postRun(String workflowUrl, Object workflowParams, String workflowType) {
+  public ResponseEntity<RunStateResponse> postRun(String workflowUrl, Object workflowParams) {
 
     ApiClient client = new ApiClient();
     client.setBasePath(this.cromwellConfig.baseUri());
