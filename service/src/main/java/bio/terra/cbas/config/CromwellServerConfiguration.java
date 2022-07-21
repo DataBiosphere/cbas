894d2fe52cc0cc5e16900b1046037e6dd83dfbac
Katrina Pierre <kpierre@broadinstitute.org>
2022-07-21 14:22:29 -0400

    saving

--- service/src/main/java/bio/terra/cbas/config/CromwellServerConfiguration.java
+++ service/src/main/java/bio/terra/cbas/config/CromwellServerConfiguration.java
@@ -1,10 +1,6 @@
 package bio.terra.cbas.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.stereotype.Component;
 
-@Component
-@ConfigurationPropertiesScan
 @ConfigurationProperties(prefix = "workflow-engines.cromwell")
 public record CromwellServerConfiguration(String baseUri, String healthUri) {}
