8d04f2ceeb656e27e7cd3af210f2b615b7744f8e
Michael Spector <michael.l.spector@gmail.com>
2022-10-11 15:46:59 -0400

    add @ConstructorBinding to CbasApiConfiguration

--- service/src/main/java/bio/terra/cbas/config/CbasApiConfiguration.java
+++ service/src/main/java/bio/terra/cbas/config/CbasApiConfiguration.java
@@ -1,7 +1,9 @@
 package bio.terra.cbas.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "cbas.cbas-api")
 public class CbasApiConfiguration {
   int runSetsMaximumRecordIds;
