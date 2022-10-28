4170e1eb559139f1568cd25f809c55c09bc73371
Katrina P <68349264+kpierre13@users.noreply.github.com>
2022-10-28 11:39:50 -0400

    Update service/src/main/java/bio/terra/cbas/common/exceptions/WorkflowAttributesNotFoundException.java
    Co-authored-by: Chris Llanwarne <cjllanwarne@users.noreply.github.com>

--- service/src/main/java/bio/terra/cbas/common/exceptions/WorkflowAttributesNotFoundException.java
+++ service/src/main/java/bio/terra/cbas/common/exceptions/WorkflowAttributesNotFoundException.java
@@ -2,7 +2,7 @@ package bio.terra.cbas.common.exceptions;
 
 public class WorkflowAttributesNotFoundException extends Exception {
 
-  public WorkflowAttributesNotFoundException(String message) {
-    super(message);
+  public WorkflowAttributesNotFoundException(String attribute, String recordId, String inputName) {
+    super("Attribute %s not found in WDS record %s (to populate workflow input %s).".formatted(attribute, recordId, inputName));
   }
 }
