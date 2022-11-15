4a0bf1e92c28cdb6fcf39cb82adab4e657a5baf4
Michael Spector <michael.l.spector@gmail.com>
2022-11-15 13:28:21 -0500

    support output definitions

--- useful workflows/womtool-to-cbas.py
+++ useful workflows/womtool-to-cbas.py
@@ -6,6 +6,7 @@ and should not be considered to be actively maintained. Use at your own risk.
 
 '''
 
+
 import argparse
 import json
 import re
@@ -16,13 +17,19 @@ def get_args():
     parser.add_argument(
         "womtool_file",
         type=str,
-        help="Path to a JSON file generated from womtool's `inputs` command"
+        help="Path to a JSON file generated from womtool's `inputs` command",
     )
     parser.add_argument(
         "cbas_file",
         type=str,
-        help="The name of a file to write the resulting CBAS input definitions"
+        help="The name of a file to write the resulting CBAS input or output definitions",
     )
+    parser.add_argument(
+        "--output_def",
+        action="store_true",
+        help="If present, the results will be formatted as output definitions",
+    )
+
     return parser.parse_args()
 
 
@@ -31,8 +38,7 @@ def main():
     
     with open(args.womtool_file) as f:
         womtool_result = json.load(f)
-
-        cbas_definition = {k: womtool_to_cbas(k, v) for k, v in womtool_result.items()}
+        cbas_definition = {k: womtool_to_cbas(k, v, args.output_def) for k, v in womtool_result.items()}
 
     with open(args.cbas_file, 'w') as f:
         json.dump(cbas_definition, f, indent=2)
@@ -48,14 +54,14 @@ def parse_default_value(womtool_string):
     return default_value
 
 
-def parse_source_spec(input_name):
-    return {
+def parse_source_spec(name, record_attribute_only=False):
+    record_attribute = name.replace('.', '_')
+    return record_attribute if record_attribute_only else {
         "type": "record_lookup",
-        "record_attribute": input_name.replace('.', '_')
+        "record_attribute": record_attribute
     }
 
-
-def parse_input_type(womtool_string):
+def parse_type(womtool_string):
     # WARNING: Doesn't support type "Map" yet.    
     type_matches = re.search(
         f'({ "|".join(ALL_TYPES) })(\[(.+)?\]\+?)?',
@@ -72,7 +78,7 @@ def parse_input_type(womtool_string):
     
     type_spec = {
         "type": outer_type_key,
-        f"{outer_type_key}_type": outer_type if not inner_type else parse_input_type(inner_type)
+        f"{outer_type_key}_type": outer_type if not inner_type else parse_type(inner_type)
     }
     
     # reminder: the parentheses around '(optional)' denote a capture group, not literal parentheses
@@ -92,12 +98,16 @@ def parse_input_type(womtool_string):
     return type_spec
     
 
-def womtool_to_cbas(input_name, womtool_string):
-    return {
-        "input_name": input_name,
-        "input_type": parse_input_type(womtool_string),
-        "source": parse_source_spec(input_name)
+def womtool_to_cbas(name, womtool_string, output_def=False):
+    mode = "input" if not output_def else "output"
+    definition = {
+        f"{mode}_name": name,
+        f"{mode}_type": parse_type(womtool_string),
+        "source" if mode == "input" else "record_attribute": parse_source_spec(name, record_attribute_only=output_def),
     }
+
+    return definition
+
 
 if __name__ == "__main__":
     main()
