618ecc783488ac48f8a0d870b95709f1ebefdc09
Michael Spector <michael.l.spector@gmail.com>
2022-11-25 05:55:52 -0500

    stub of junit-only style test

---
+++ service/src/test/java/bio/terra/cbas/controllers/TestRunSetsApiControllerPact.java
@@ -0,0 +1,13 @@
+package bio.terra.cbas.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/** Placeholder test case to prove the github action integration. */
+public class TestRunSetsApiControllerPact {
+  @Test
+  void addition() {
+    assertEquals(2, 1 + 1);
+  }
+}
