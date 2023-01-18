a25a69b003d44bff030a9267468f8e90771ed89f
Katrina Pierre <kpierre@broadinstitute.org>
2023-01-18 14:53:03 -0500

    spotless

--- service/src/main/java/bio/terra/cbas/dao/MethodVersionDao.java
+++ service/src/main/java/bio/terra/cbas/dao/MethodVersionDao.java
@@ -7,7 +7,6 @@ import bio.terra.cbas.models.RunSet;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
