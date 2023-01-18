7a178baa474bed34cf15709aaf8703e806b3cf66
Katrina Pierre <kpierre@broadinstitute.org>
2023-01-18 12:44:07 -0500

    MethodVersionDao tests working

--- service/src/test/java/bio/terra/cbas/dao/TestMethodVersionDao.java
+++ service/src/test/java/bio/terra/cbas/dao/TestMethodVersionDao.java
@@ -1,43 +1,70 @@
 package bio.terra.cbas.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bio.terra.cbas.models.Method;
 import bio.terra.cbas.models.MethodVersion;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestMethodVersionDao {
 
-  MethodVersionDao methodVersionDao;
+  @Autowired MethodVersionDao methodVersionDao;
+
+  UUID methodVersionId = UUID.fromString("20000000-0000-0000-0000-000000000002");
+
+  UUID methodId = UUID.fromString("00000000-0000-0000-0000-000000000002");
+  String time = "2023-01-10T16:46:23.946326Z";
+
+  String name = "Target workflow 2";
+  String description = "Add description";
+
+  Method dbMethod =
+      new Method(methodId, name, description, OffsetDateTime.parse(time), null, "Dockstore-Github");
 
   @Test
   void retrievesSingleMethodVersion() {
-    UUID methodVersionId = UUID.randomUUID();
+
+    MethodVersion methodVersion =
+        new MethodVersion(
+            methodVersionId,
+            dbMethod,
+            "1.0",
+            "First version of target workflow 2",
+            OffsetDateTime.parse("2023-01-10T16:46:23.955430Z"),
+            null,
+            "https://raw.githubusercontent.com/DataBiosphere/cbas/main/useful_workflows/target_workflow_2/target_workflow_2.wdl");
 
     MethodVersion actual = methodVersionDao.getMethodVersion(methodVersionId);
 
-    // Same as db?
-    // MethodVersion expected = new MethodVersion(...);
-
-    // assertEquals(expected, actual);
+    assertEquals(methodVersion, actual);
   }
 
   @Test
   void retrievesAllMethodVersions() {
-    //    Method method = new MethodDetails()
-    //        .method_id(UUID.randomUUID())
-    //        .name("methodVersionName")
-    //
-    //
-    //        String name,
-    //        String description,
-    //        OffsetDateTime created,
-    //        UUID lastRunSetId,
-    //        String methodSource
 
-    // List<MethodVersion> actual = methodVersionDao.getMethodVersionsForMethod(method);
+    List<MethodVersion> methodVersions = new ArrayList<>();
 
-    // Same as db?
-    // MethodVersion expected = new MethodVersion(...);
+    methodVersions.add(
+        new MethodVersion(
+            UUID.fromString("20000000-0000-0000-0000-000000000002"),
+            dbMethod,
+            "1.0",
+            "First version of target workflow 2",
+            OffsetDateTime.parse("2023-01-10T16:46:23.955430Z"),
+            null,
+            "https://raw.githubusercontent.com/DataBiosphere/cbas/main/useful_workflows/target_workflow_2/target_workflow_2.wdl"));
 
-    // assertEquals(expected, actual);
+    List<MethodVersion> actual = methodVersionDao.getMethodVersionsForMethod(dbMethod);
+
+    assertEquals(methodVersions, actual);
   }
 }
