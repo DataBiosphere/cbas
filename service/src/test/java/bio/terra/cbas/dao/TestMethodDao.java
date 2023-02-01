c83cf8d344ca93d41dfc9b9975d4decaa0fce4ad
Katrina Pierre <kpierre@broadinstitute.org>
2023-02-01 11:44:52 -0500

    Updating build and test and methoddao test

--- service/src/test/java/bio/terra/cbas/dao/TestMethodDao.java
+++ service/src/test/java/bio/terra/cbas/dao/TestMethodDao.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import bio.terra.cbas.models.Method;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -46,33 +45,6 @@ class TestMethodDao {
 
   @Test
   void retrievesAllMethods() {
-
-    List<Method> allMethods = new ArrayList<>();
-
-    allMethods.add(
-        new Method(
-            UUID.fromString("00000000-0000-0000-0000-000000000005"),
-            "assemble_refbased",
-            "assemble_refbased",
-            OffsetDateTime.parse("2023-01-27T19:21:24.542692Z"),
-            null,
-            "Github"));
-    allMethods.add(
-        new Method(
-            UUID.fromString("00000000-0000-0000-0000-000000000006"),
-            "sarscov2_nextstrain",
-            "sarscov2_nextstrain",
-            OffsetDateTime.parse("2023-01-27T19:21:24.552878Z"),
-            null,
-            "Github"));
-    allMethods.add(
-        new Method(
-            UUID.fromString("00000000-0000-0000-0000-000000000008"),
-            "fetch_sra_to_bam",
-            "fetch_sra_to_bam",
-            OffsetDateTime.parse("2023-01-27T19:21:24.563932Z"),
-            null,
-            "Github"));
 
     List<Method> actual = methodDao.getMethods();
 
