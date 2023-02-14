b208743fc916d0627c32df13cd36a66171817564
Saloni Shah <salonipshah11@gmail.com>
2023-02-14 17:54:22 -0500

    PR feedback

--- service/src/test/java/bio/terra/cbas/dao/TestRunSetDao.java
+++ service/src/test/java/bio/terra/cbas/dao/TestRunSetDao.java
@@ -79,5 +79,8 @@ class TestRunSetDao {
     // DB has no non-templated run sets, so it will return empty list
     List<RunSet> runSets = runSetDao.getRunSets(10, false);
     assertEquals(0, runSets.size());
+
+    List<RunSet> templateRunSets = runSetDao.getRunSets(10, true);
+    assertEquals(3, templateRunSets.size());
   }
 }
