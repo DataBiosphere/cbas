c7e6baa38793031dd586ab341c5af04940805abe
Chris Llanwarne <cjllanwarne@gmail.com>
2023-02-24 17:30:37 -0500

    Struct type definitions in API

--- service/src/test/java/bio/terra/cbas/runsets/types/TestDefinitionParsing.java
+++ service/src/test/java/bio/terra/cbas/runsets/types/TestDefinitionParsing.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import bio.terra.cbas.model.ParameterTypeDefinition;
 import bio.terra.cbas.model.ParameterTypeDefinitionMap;
 import bio.terra.cbas.model.ParameterTypeDefinitionPrimitive;
+import bio.terra.cbas.model.ParameterTypeDefinitionStruct;
 import bio.terra.cbas.model.PrimitiveParameterValueType;
+import bio.terra.cbas.model.StructField;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class TestDefinitionParsing {
@@ -39,6 +42,84 @@ class TestDefinitionParsing {
             .type(ParameterTypeDefinition.TypeEnum.MAP);
     ParameterTypeDefinition actualType =
         objectMapper.readValue(mapDefinition, new TypeReference<>() {});
+    assertEquals(expectedType, actualType);
+  }
+
+  @Test
+  void parseStructDefinition() throws Exception {
+    String structDefinition =
+        """
+        {
+          "type": "struct",
+          "name": "Pet",
+          "fields": [{
+            "field_name": "name",
+            "field_type": {
+              "type": "primitive",
+              "primitive_type": "String"
+            }
+          }, {
+            "field_name": "species",
+            "field_type": {
+              "type": "struct",
+              "name": "PetSpecies",
+              "fields": [{
+                "field_name": "species_name",
+                "field_type": {
+                  "type": "primitive",
+                  "primitive_type": "String"
+                }
+              }, {
+                "field_name": "breed_name",
+                "field_type": {
+                  "type": "primitive",
+                  "primitive_type": "String"
+                }
+              }]
+            }
+          }]
+        }""";
+
+    ParameterTypeDefinition expectedType =
+        new ParameterTypeDefinitionStruct()
+            .name("Pet")
+            .fields(
+                List.of(
+                    new StructField()
+                        .fieldName("name")
+                        .fieldType(
+                            new ParameterTypeDefinitionPrimitive()
+                                .primitiveType(PrimitiveParameterValueType.STRING)
+                                .type(ParameterTypeDefinition.TypeEnum.PRIMITIVE)),
+                    new StructField()
+                        .fieldName("species")
+                        .fieldType(
+                            new ParameterTypeDefinitionStruct()
+                                .name("PetSpecies")
+                                .fields(
+                                    List.of(
+                                        new StructField()
+                                            .fieldName("species_name")
+                                            .fieldType(
+                                                new ParameterTypeDefinitionPrimitive()
+                                                    .primitiveType(
+                                                        PrimitiveParameterValueType.STRING)
+                                                    .type(
+                                                        ParameterTypeDefinition.TypeEnum
+                                                            .PRIMITIVE)),
+                                        new StructField()
+                                            .fieldName("breed_name")
+                                            .fieldType(
+                                                new ParameterTypeDefinitionPrimitive()
+                                                    .primitiveType(
+                                                        PrimitiveParameterValueType.STRING)
+                                                    .type(
+                                                        ParameterTypeDefinition.TypeEnum
+                                                            .PRIMITIVE))))
+                                .type(ParameterTypeDefinition.TypeEnum.STRUCT))))
+            .type(ParameterTypeDefinition.TypeEnum.STRUCT);
+    ParameterTypeDefinition actualType =
+        objectMapper.readValue(structDefinition, new TypeReference<>() {});
     assertEquals(expectedType, actualType);
   }
 }
