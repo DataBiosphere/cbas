8a2c5fd35a1c1c1b838408268031483a9f0ff940
Michael Spector <michael.l.spector@gmail.com>
2023-04-03 17:08:55 -0400

    separate verifyPacts and unitTests

--- service/src/test/java/bio/terra/cbas/controllers/TestRunSetsApiController.java
+++ service/src/test/java/bio/terra/cbas/controllers/TestRunSetsApiController.java
@@ -24,12 +24,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import au.com.dius.pact.provider.junit5.PactVerificationContext;
-import au.com.dius.pact.provider.junitsupport.Provider;
-import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
-import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import bio.terra.cbas.config.CbasApiConfiguration;
 import bio.terra.cbas.dao.MethodDao;
 import bio.terra.cbas.dao.MethodVersionDao;
@@ -66,8 +60,6 @@ import org.databiosphere.workspacedata.model.RecordAttributes;
 import org.databiosphere.workspacedata.model.RecordResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -77,133 +69,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-@WebMvcTest
-@ContextConfiguration(classes = {RunSetsApiController.class, CbasApiConfiguration.class})
-@Provider("cbas")
-@PactBroker()
-class TestRunSetsApiControllerPacts {
-  private static final String API = "/api/batch/v1/run_sets";
-
-  @MockBean private CromwellService cromwellService;
-  @MockBean private WdsService wdsService;
-  @MockBean private MethodDao methodDao;
-  @MockBean private MethodVersionDao methodVersionDao;
-  @MockBean private RunSetDao runSetDao;
-  @MockBean private RunDao runDao;
-  @MockBean private SmartRunSetsPoller smartRunSetsPoller;
-  @Autowired private ObjectMapper objectMapper;
-
-  // This mockMVC is what we use to test API requests and responses:
-  @Autowired private MockMvc mockMvc;
-
-  @TestTemplate
-  @ExtendWith(PactVerificationSpringProvider.class)
-  void pactVerificationTestTemplate(PactVerificationContext context) {
-    context.verifyInteraction();
-  }
-
-  @BeforeEach
-  void before(PactVerificationContext context) {
-    context.setTarget(new MockMvcTestTarget(mockMvc));
-  }
-
-  @State({"at least one run set exists with method_id 00000000-0000-0000-0000-000000000009"})
-  public void runSetsData() throws Exception {
-    Method myMethod =
-        new Method(
-            UUID.fromString("00000000-0000-0000-0000-000000000009"),
-            "myMethod name",
-            "myMethod description",
-            OffsetDateTime.now(),
-            UUID.fromString("90000000-0000-0000-0000-000000000009"),
-            "myMethod source");
-
-    MethodVersion myMethodVersion =
-        new MethodVersion(
-            UUID.randomUUID(),
-            myMethod,
-            "myMethodVersion name",
-            "myMethodVersion description",
-            OffsetDateTime.now(),
-            UUID.randomUUID(),
-            "http://myMethodVersionUrl.com");
-    RunSet runSetError =
-        new RunSet(
-            UUID.randomUUID(),
-            myMethodVersion,
-            "runSetError",
-            "a run set with error status",
-            false,
-            CbasRunSetStatus.ERROR,
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            1,
-            1,
-            "my input definition string",
-            "my output definition string",
-            "myRecordType");
-
-    RunSet runSetComplete =
-        new RunSet(
-            UUID.randomUUID(),
-            myMethodVersion,
-            "runSetError",
-            "a run set with error status",
-            false,
-            CbasRunSetStatus.COMPLETE,
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            1,
-            1,
-            "my input definition string",
-            "my output definition string",
-            "myRecordType");
-
-    RunSet runSetUnknown =
-        new RunSet(
-            UUID.randomUUID(),
-            myMethodVersion,
-            "runSetError",
-            "a run set with error status",
-            false,
-            CbasRunSetStatus.UNKNOWN,
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            1,
-            1,
-            "my input definition string",
-            "my output definition string",
-            "myRecordType");
-
-    RunSet runSetRunning =
-        new RunSet(
-            UUID.randomUUID(),
-            myMethodVersion,
-            "runSetError",
-            "a run set with error status",
-            false,
-            CbasRunSetStatus.RUNNING,
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            OffsetDateTime.now(),
-            1,
-            1,
-            "my input definition string",
-            "my output definition string",
-            "myRecordType");
-
-    List<RunSet> response = List.of(runSetError, runSetComplete, runSetUnknown, runSetRunning);
-
-    when(runSetDao.getRunSets(any(), eq(false))).thenReturn(response);
-
-    when(smartRunSetsPoller.updateRunSets(response))
-        .thenReturn(new TimeLimitedUpdater.UpdateResult<>(response, 4, 4, true));
-  }
-}
 
 @WebMvcTest
 @TestPropertySource(properties = "cbas.cbas-api.runSetsMaximumRecordIds=3")
