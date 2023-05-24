11e4f1ace684c78d36a4687d9569fe6d250c320f
Saloni Shah <salonipshah11@gmail.com>
2023-05-24 18:57:54 -0400

    spotless fix

--- service/src/test/java/bio/terra/cbas/controllers/TestRunSetsApiController.java
+++ service/src/test/java/bio/terra/cbas/controllers/TestRunSetsApiController.java
@@ -10,7 +10,6 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
