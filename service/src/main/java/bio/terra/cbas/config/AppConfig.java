43a85096410e899507844322378103d858adfa2a
Chris Llanwarne <cjllanwarne@gmail.com>
2023-05-25 14:14:25 -0400

    Remove unused

--- service/src/main/java/bio/terra/cbas/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package bio.terra.cbas.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.EnableRetry;
-
-@Configuration
-@EnableRetry
-public class AppConfig {}
--- service/src/main/java/bio/terra/cbas/config/CbasSchedulerConfiguration.java
+++
@@ -1,6 +0,0 @@
-package bio.terra.cbas.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "cbas.scheduler")
-public record CbasSchedulerConfiguration(int healthCheckIntervalSeconds) {}
