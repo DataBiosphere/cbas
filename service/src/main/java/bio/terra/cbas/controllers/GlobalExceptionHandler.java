742dfc7223a61e293e61f5b0aa85602ec8f038c8
dillydally414 <67511512+dillydally414@users.noreply.github.com>
2023-08-09 16:21:54 -0400

    spotless

--- service/src/main/java/bio/terra/cbas/controllers/GlobalExceptionHandler.java
+++ service/src/main/java/bio/terra/cbas/controllers/GlobalExceptionHandler.java
@@ -4,8 +4,8 @@ import bio.terra.cbas.config.BeanConfig;
 import bio.terra.cbas.model.ErrorReport;
 import bio.terra.common.exception.AbstractGlobalExceptionHandler;
 import bio.terra.common.exception.ErrorReportException;
-import java.util.List;
 import bio.terra.common.iam.BearerToken;
+import java.util.List;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +22,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends AbstractGlobalExceptionHandler<ErrorReport> {
   /**
-   * This method allows for error uncovering of custom exceptions that we throw inside bean creation,
-   * especially the {@link BearerToken} bean in {@link BeanConfig}. If the root cause does not stem
-   * from our own custom errors, it will be reported by our catch all handler as an internal server
-   * error.
+   * This method allows for error uncovering of custom exceptions that we throw inside bean
+   * creation, especially the {@link BearerToken} bean in {@link BeanConfig}. If the root cause does
+   * not stem from our own custom errors, it will be reported by our catch all handler as an
+   * internal server error.
    */
   @ExceptionHandler(BeanCreationException.class)
   public ResponseEntity<ErrorReport> beanCreationErrorHandler(BeanCreationException ex) {
