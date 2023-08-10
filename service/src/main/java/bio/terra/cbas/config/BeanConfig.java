d04605e044204e9e165f54a1e92436973252d010
Dillon Scott <67511512+dillydally414@users.noreply.github.com>
2023-08-10 10:24:43 -0400

    [WM-2178] CBAS ignores unknown properties in request body (#189)

--- service/src/main/java/bio/terra/cbas/config/BeanConfig.java
+++ service/src/main/java/bio/terra/cbas/config/BeanConfig.java
@@ -3,6 +3,7 @@ package bio.terra.cbas.config;
 import bio.terra.common.iam.BearerToken;
 import bio.terra.common.iam.BearerTokenFactory;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -25,7 +26,8 @@ public class BeanConfig {
         .registerModule(new Jdk8Module())
         .registerModule(new JavaTimeModule())
         .setDateFormat(new StdDateFormat())
-        .setDefaultPropertyInclusion(JsonInclude.Include.NON_ABSENT);
+        .setDefaultPropertyInclusion(JsonInclude.Include.NON_ABSENT)
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
   }
 
   /**
