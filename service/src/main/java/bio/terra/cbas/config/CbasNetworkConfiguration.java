471b1b59142cab003ce56af41398d8ce52258970
Chris Llanwarne <cjllanwarne@gmail.com>
2023-09-25 15:22:06 -0400

    Request callback in submissions to Cromwell

---
+++ service/src/main/java/bio/terra/cbas/config/CbasNetworkConfiguration.java
@@ -0,0 +1,28 @@
+package bio.terra.cbas.config;
+
+import java.util.Optional;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "cbas.network")
+public class CbasNetworkConfiguration {
+
+  public String externalUri;
+
+  public Optional<String> getCallbackUri() {
+    if (externalUri == null || externalUri.isEmpty()) {
+      return Optional.empty();
+    } else {
+      return Optional.of(externalUri + "api/batch/v1/runs/results");
+    }
+  }
+
+  public String getExternalUri() {
+    return externalUri;
+  }
+
+  public void setExternalUri(String externalUri) {
+    this.externalUri = externalUri;
+  }
+}
--- service/src/test/java/bio/terra/cbas/controllers/TestRunSetsApiController.java
+++ service/src/test/java/bio/terra/cbas/controllers/TestRunSetsApiController.java
@@ -28,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import bio.terra.cbas.common.exceptions.ForbiddenException;
 import bio.terra.cbas.config.CbasApiConfiguration;
+import bio.terra.cbas.config.CbasNetworkConfiguration;
 import bio.terra.cbas.config.CromwellServerConfiguration;
 import bio.terra.cbas.config.LeonardoServerConfiguration;
 import bio.terra.cbas.dao.MethodDao;
@@ -811,16 +812,49 @@ class TestRunSetsApiController {
     DependencyUrlLoader dependencyUrlLoader =
         new DependencyUrlLoader(leonardoService, appUtils, leonardoServerConfiguration);
     CromwellClient localTestClient = new CromwellClient(localTestConfig, dependencyUrlLoader);
-    CromwellService localtestService = new CromwellService(localTestClient, cromwellClient);
+    CbasNetworkConfiguration cbasNetworkConfiguration = new CbasNetworkConfiguration();
+    cbasNetworkConfiguration.setExternalUri("http://localhost:8080");
+    CromwellService localtestService =
+        new CromwellService(localTestClient, cromwellClient, cbasNetworkConfiguration);
 
     // Workflow options should reflect the final workflow log directory.
     // write_to_cache should always be true. read_from_cache should match the provided call caching
     // option.
     String expected =
-        "{\"final_workflow_log_dir\":\"my/final/workflow/log/dir\",\"read_from_cache\":true,\"write_to_cache\":true}";
+        "{\"final_workflow_log_dir\":\"my/final/workflow/log/dir\",\"read_from_cache\":true,\"workflow_callback_uri\":\"http://localhost:8080api/batch/v1/runs/results\",\"write_to_cache\":true}";
     assertEquals(expected, localtestService.buildWorkflowOptionsJson(true));
     String expectedFalse =
-        "{\"final_workflow_log_dir\":\"my/final/workflow/log/dir\",\"read_from_cache\":false,\"write_to_cache\":true}";
+        "{\"final_workflow_log_dir\":\"my/final/workflow/log/dir\",\"read_from_cache\":false,\"workflow_callback_uri\":\"http://localhost:8080api/batch/v1/runs/results\",\"write_to_cache\":true}";
+    assertEquals(expectedFalse, localtestService.buildWorkflowOptionsJson(false));
+
+    // Test that the workflow options are properly constructed when the external uri is not set.
+    cbasNetworkConfiguration.setExternalUri(null);
+    String expectedNoExternalUri =
+        "{\"final_workflow_log_dir\":\"my/final/workflow/log/dir\",\"read_from_cache\":true,\"write_to_cache\":true}";
+    assertEquals(expectedNoExternalUri, localtestService.buildWorkflowOptionsJson(true));
+  }
+
+  @Test
+  void testResilientToMissingExternalUri() {
+    CromwellServerConfiguration localTestConfig =
+        new CromwellServerConfiguration("my/base/uri", null, "my/final/workflow/log/dir", false);
+    var leonardoServerConfiguration =
+        new LeonardoServerConfiguration("", List.of(), List.of(), Duration.ofMinutes(10), false);
+    DependencyUrlLoader dependencyUrlLoader =
+        new DependencyUrlLoader(leonardoService, appUtils, leonardoServerConfiguration);
+    CromwellClient localTestClient = new CromwellClient(localTestConfig, dependencyUrlLoader);
+    CbasNetworkConfiguration cbasNetworkConfiguration = new CbasNetworkConfiguration();
+    CromwellService localtestService =
+        new CromwellService(localTestClient, cromwellClient, cbasNetworkConfiguration);
+
+    // Workflow options should reflect the final workflow log directory.
+    // write_to_cache should always be true. read_from_cache should match the provided call caching
+    // option.
+    String expected =
+        "{\"final_workflow_log_dir\":\"my/final/workflow/log/dir\",\"read_from_cache\":true,\"workflow_callback_uri\":\"http://localhost:8080api/batch/v1/runs/results\",\"write_to_cache\":true}";
+    assertEquals(expected, localtestService.buildWorkflowOptionsJson(true));
+    String expectedFalse =
+        "{\"final_workflow_log_dir\":\"my/final/workflow/log/dir\",\"read_from_cache\":false,\"workflow_callback_uri\":\"http://localhost:8080api/batch/v1/runs/results\",\"write_to_cache\":true}";
     assertEquals(expectedFalse, localtestService.buildWorkflowOptionsJson(false));
   }
 
