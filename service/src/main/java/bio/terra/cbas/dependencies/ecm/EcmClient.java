bb8693af21f2e8556a1bbe87e95479a0757b2536
Katrina Pierre <kpierre@broadinstitute.org>
2024-03-07 10:23:16 -0500

    ecm implemented and returns health check

---
+++ service/src/main/java/bio/terra/cbas/dependencies/ecm/EcmClient.java
@@ -0,0 +1,45 @@
+package bio.terra.cbas.dependencies.ecm;
+
+import bio.terra.cbas.config.EcmServerConfiguration;
+import bio.terra.externalcreds.api.OauthApi;
+import bio.terra.externalcreds.api.PublicApi;
+import bio.terra.externalcreds.client.ApiClient;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EcmClient {
+  private final EcmServerConfiguration ecmServerConfiguration;
+
+  public EcmClient(EcmServerConfiguration ecmServerConfiguration) {
+    this.ecmServerConfiguration = ecmServerConfiguration;
+  }
+
+  public ApiClient apiClient() {
+    ApiClient apiClient = new ApiClient();
+
+    apiClient.setBasePath(ecmServerConfiguration.baseUri());
+    apiClient.addDefaultHeader("Connection", "close");
+    apiClient.setDebugging(ecmServerConfiguration.debugApiLogging());
+
+    return apiClient;
+  }
+
+  public ApiClient apiAuthClient(String accessToken) {
+    ApiClient apiClient = new ApiClient();
+
+    apiClient.setBasePath(ecmServerConfiguration.baseUri());
+    apiClient.setAccessToken(accessToken);
+    apiClient.addDefaultHeader("Connection", "close");
+    apiClient.setDebugging(ecmServerConfiguration.debugApiLogging());
+
+    return apiClient;
+  }
+
+  public OauthApi oAuthApi(ApiClient apiClient) {
+    return new OauthApi(apiClient);
+  }
+
+  public PublicApi publicApi(ApiClient apiClient) {
+    return new PublicApi(apiClient);
+  }
+}
