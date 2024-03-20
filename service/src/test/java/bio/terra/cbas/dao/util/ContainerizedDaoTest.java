cd1952459037b7d6a549794d4ef668803227e7f1
Michael Spector <michael.l.spector@gmail.com>
2024-03-20 15:53:51 -0400

    Revert "use test profile to avoid problems with empty workspaceCreatedDate value" This is an experiment to see if ActiveProfiles is interfering with the integration tests. This reverts commit 42dfd33aeee2a39d800a4ce7faeb53a8c853859d.

--- service/src/test/java/bio/terra/cbas/dao/util/ContainerizedDaoTest.java
+++ service/src/test/java/bio/terra/cbas/dao/util/ContainerizedDaoTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
@@ -14,8 +13,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(properties = {"spring.main.allow-bean-definition-overriding=true"})
 // Without @DirtiesContext, multiple ContainerizedDaoTest classes executed in rapid succession
 // can cause loss of connection to the containerized database.
 // See: https://stackoverflow.com/a/68992727
--- service/src/test/java/bio/terra/cbas/dependencies/sam/TestSamService.java
+++ service/src/test/java/bio/terra/cbas/dependencies/sam/TestSamService.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
+import bio.terra.cbas.dao.util.ContainerizedDaoTest;
 import bio.terra.common.exception.UnauthorizedException;
 import bio.terra.common.iam.BearerToken;
 import bio.terra.common.sam.exception.SamInterruptedException;
@@ -41,7 +42,6 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
 
 class TestSamService {
   private SamClient samClient;
@@ -341,11 +341,10 @@ class TestSamService {
   @SpringBootTest(
       properties = {
         "spring.profiles.active=human-readable-logging",
-        "spring.main.allow-bean-definition-overriding: true",
-        "cbas.context.workspaceCreatedDate: 1990-12-20T00:00:00.000000Z"
+        "spring.main.allow-bean-definition-overriding=true"
       })
   @ExtendWith(OutputCaptureExtension.class)
-  class TestSamReadableLogs {
+  class TestSamReadableLogs extends ContainerizedDaoTest {
 
     @Test
     void testUserIdInLogsWithNoAccess(CapturedOutput output) {
@@ -375,10 +374,10 @@ class TestSamService {
   }
 
   @Nested
-  @SpringBootTest
-  @ActiveProfiles("test")
+  @SpringBootTest(
+      properties = {"spring.profiles.active=", "spring.main.allow-bean-definition-overriding=true"})
   @ExtendWith(OutputCaptureExtension.class)
-  class TestSamPlainLogs {
+  class TestSamPlainLogs extends ContainerizedDaoTest {
     // These 2 tests will fail when run locally because for local testing Spring default's to
     // 'human-readable-logging'. But in CI the format is not human-readable by default and hence the
     // test assertions are met as expected.
