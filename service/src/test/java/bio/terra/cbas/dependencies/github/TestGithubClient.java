1cd163ab5aac4059cad4d90110b366382c2b9ec8
Katrina Pierre <kpierre@broadinstitute.org>
2024-04-01 15:55:09 -0400

    wip

--- service/src/test/java/bio/terra/cbas/dependencies/github/TestGithubClient.java
+++ service/src/test/java/bio/terra/cbas/dependencies/github/TestGithubClient.java
@@ -1,47 +1,87 @@
 package bio.terra.cbas.dependencies.github;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.google.gson.Gson;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 class TestGithubClient {
-  @Mock GitHubClient gitHubClient;
+  private static final com.fasterxml.jackson.databind.ObjectMapper objectMapper =
+      new com.fasterxml.jackson.databind.ObjectMapper()
+          .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  @Mock GitHubClient gitHubClientMock;
   @Mock Gson gson = new Gson();
 
-  //  @Test
-  //  void getsTargetGivenOrg() throws Exception {
-  //    String org = "broad";
-  //    String repo = "cromwell";
-  //    Client client = mock(Client.class);
-  //    WebTarget mockTarget = mock(WebTarget.class);
-  //    Invocation.Builder mockBuilder = mock(Invocation.Builder.class);
-  //    GitHubClient gitHubClient = new GitHubClient();
-  //
-  //    GitHubClient.RepoInfo info = new GitHubClient.RepoInfo();
-  //    info.url("www.iurl.com");
-  //    info.isPrivate(false);
-  //    info.id("abc123");
-  //
-  //    Response mockResponse =
-  //        mockTarget
-  //            .request(MediaType.APPLICATION_JSON_TYPE)
-  //            .header("Accept", "application/vnd.github+json")
-  //            .header("X-GitHub-Api-Version", "2022-11-28")
-  //            .get();
-  //
-  //    // when(client.target("/test/me")).thenReturn(mockTarget);
-  //
-  //    when(mockResponse.getStatus()).thenReturn(200);
-  //    assertFalse(gitHubClient.getRepo(org, repo, "").getIsPrivate());
-  //    // GitHubClient.RepoInfo responseEntity =
-  // mockResponse.readEntity(GitHubClient.RepoInfo.class);
-  //  }
+  @Test
+  void getsTargetGivenOrg() throws Exception {
+    String org = "broad";
+    String repo = "cromwell";
+    Client client = mock(Client.class);
+    WebTarget mockTarget = mock(WebTarget.class);
+    Invocation.Builder mockBuilder = mock(Invocation.Builder.class);
+    GitHubClient gitHubClient = new GitHubClient();
+    MultivaluedMap<String, Object> headerMap = new MultivaluedHashMap<>();
+    Response resp = mock(Response.class);
+    String response =
+        """
+        {
+          "id": "abc123",
+          "private": "false",
+          "url": "www.urlfoo.com"
+        }
+        """
+            .trim();
+
+    // Response newResponse = objectMapper.readValue(response, Response.class);
+
+    // when(client.target("/test/target")).thenReturn(mockTarget);
+    // when(gitHubClientMock.getHeaders("")).thenReturn(headerMap);
+    when(mockTarget.request(MediaType.APPLICATION_JSON_TYPE)).thenReturn(mockBuilder);
+    // when(mockTarget.request(MediaType.APPLICATION_JSON_TYPE).get()).thenReturn(resp);
+    // when(resp.getStatus()).thenReturn(200);
+    // when(resp.readEntity(String.class)).thenReturn(response);
+
+    GitHubClient.RepoInfo info = new GitHubClient.RepoInfo();
+    info.url("www.iurl.com");
+    info.isPrivate(false);
+    info.id("abc123");
+    //    when(mockTarget
+    //            .request(MediaType.APPLICATION_JSON_TYPE)
+    //            .header("header", "one")
+    //            .header("header", "two"))
+    //        .thenReturn(mockBuilder);
+
+    //    Response mockResponse =
+    //        mockTarget
+    //            .request(MediaType.APPLICATION_JSON_TYPE)
+    //            .header("Accept", "application/vnd.github+json")
+    //            .header("X-GitHub-Api-Version", "2022-11-28")
+    //            .get();
+
+    // when(client.target("/test/me")).thenReturn(mockTarget);
+
+    // GitHubClient.RepoInfo repoInfo = gitHubClientMock.getRepo(org, repo, "");
+    // when(gitHubClientMock.getRepo(org, repo, "")).thenReturn(info);
+    assertFalse(info.getIsPrivate());
+    // GitHubClient.RepoInfo responseEntity =
+    // mockResponse.readEntity(GitHubClient.RepoInfo.class);
+  }
 
   @Test
   void getsCorrectNumberOfHeadersWithEmptyToken() {
