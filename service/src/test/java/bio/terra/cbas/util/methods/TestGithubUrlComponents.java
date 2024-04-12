4a624ffc8874def2c901078c18c9a20f1be9826d
Katrina Pierre <kpierre@broadinstitute.org>
2024-04-12 11:27:18 -0400

    add negative test case

--- service/src/test/java/bio/terra/cbas/util/methods/TestGithubUrlComponents.java
+++ service/src/test/java/bio/terra/cbas/util/methods/TestGithubUrlComponents.java
@@ -1,6 +1,7 @@
 package bio.terra.cbas.util.methods;
 
 import static bio.terra.cbas.common.MethodUtil.extractGithubDetailsFromUrl;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URISyntaxException;
@@ -9,7 +10,9 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 class TestGithubUrlComponents {
-
+  // assertThrows(
+  //            CustomChangeException.class, () ->
+  // backfillGithubDetails.generateStatements(mockDb));
   record TestCase(String org, String repo, String branchOrTag, String path, String url) {
 
     public void check() throws URISyntaxException {
@@ -17,6 +20,10 @@ class TestGithubUrlComponents {
       assertEquals(extractGithubDetailsFromUrl(url).path(), path);
       assertEquals(extractGithubDetailsFromUrl(url).branchOrTag(), branchOrTag);
       assertEquals(extractGithubDetailsFromUrl(url).repo(), repo);
+    }
+
+    public void UriExceptionCheck() {
+      assertThrows(URISyntaxException.class, () -> extractGithubDetailsFromUrl(url));
     }
   }
 
@@ -40,5 +47,14 @@ class TestGithubUrlComponents {
         };
 
     return DynamicTest.stream(Stream.of(testCases), TestCase::url, TestCase::check);
+  }
+
+  @TestFactory
+  Stream<DynamicTest> throwsUriException() {
+    var testCases =
+        new TestCase[] {
+          new TestCase(null, null, null, null, "not a url"),
+        };
+    return DynamicTest.stream(Stream.of(testCases), TestCase::url, TestCase::UriExceptionCheck);
   }
 }
