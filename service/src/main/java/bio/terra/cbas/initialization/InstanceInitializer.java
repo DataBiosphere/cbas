73930a76330faf9efb7fcc3ae2df9641c2ddc8f3
Michael Spector <michael.l.spector@gmail.com>
2024-04-22 13:23:21 -0400

    remove more unused code

--- service/src/main/java/bio/terra/cbas/initialization/InstanceInitializer.java
+++ service/src/main/java/bio/terra/cbas/initialization/InstanceInitializer.java
@@ -1,28 +1,21 @@
 package bio.terra.cbas.initialization;
 
-import bio.terra.cbas.config.CbasContextConfiguration;
 import bio.terra.cbas.config.CbasInitializationConfiguration;
 import bio.terra.cbas.initialization.cloneRecovery.CloneRecoveryService;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InstanceInitializer implements ApplicationListener<ContextRefreshedEvent> {
-  private final Logger logger = LoggerFactory.getLogger(InstanceInitializer.class);
   private final CloneRecoveryService cloneRecoveryService;
   private final CbasInitializationConfiguration cbasInitializationConfiguration;
-  private final CbasContextConfiguration cbasContextConfiguration;
 
   public InstanceInitializer(
       CbasInitializationConfiguration cbasInitializationConfiguration,
-      CbasContextConfiguration cbasContextConfiguration,
       CloneRecoveryService cloneRecoveryService) {
     this.cbasInitializationConfiguration = cbasInitializationConfiguration;
-    this.cbasContextConfiguration = cbasContextConfiguration;
     this.cloneRecoveryService = cloneRecoveryService;
   }
 
