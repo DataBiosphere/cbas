4d67855e53f5543fea7e0fb0f1b4aa64dd7b9b24
Saloni Shah <salonipshah11@gmail.com>
2024-04-23 12:02:52 -0400

    add BearerTokenFactory mock bean

--- service/src/test/java/bio/terra/cbas/pact/VerifyPactsAllControllers.java
+++ service/src/test/java/bio/terra/cbas/pact/VerifyPactsAllControllers.java
@@ -46,6 +46,7 @@ import bio.terra.cbas.runsets.monitoring.SmartRunsPoller;
 import bio.terra.cbas.runsets.results.RunCompletionHandler;
 import bio.terra.cbas.runsets.results.RunCompletionResult;
 import bio.terra.cbas.util.UuidSource;
+import bio.terra.common.iam.BearerTokenFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cromwell.client.model.WorkflowDescription;
 import cromwell.client.model.WorkflowIdAndStatus;
@@ -109,6 +110,7 @@ class VerifyPactsAllControllers {
   @MockBean private GithubMethodDetailsDao githubMethodDetailsDao;
   @MockBean private GitHubService gitHubService;
   @MockBean private EcmService ecmService;
+  @MockBean private BearerTokenFactory bearerTokenFactory;
 
   // This mockMVC is what we use to test API requests and responses:
   @Autowired private MockMvc mockMvc;
