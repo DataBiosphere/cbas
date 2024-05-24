1b2c3880586de873ca4a4838b76839fa93af1c42
Michael Spector <michael.l.spector@gmail.com>
2024-05-24 09:13:51 -0400

    sonarcloud issues

--- service/src/main/java/bio/terra/cbas/service/MethodService.java
+++ service/src/main/java/bio/terra/cbas/service/MethodService.java
@@ -1,32 +1,15 @@
 package bio.terra.cbas.service;
 
-import bio.terra.cbas.dao.GithubMethodDetailsDao;
 import bio.terra.cbas.dao.MethodDao;
-import bio.terra.cbas.dao.MethodVersionDao;
-import bio.terra.cbas.dao.RunDao;
-import bio.terra.cbas.dao.RunSetDao;
 import java.util.UUID;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MethodService {
   private final MethodDao methodDao;
-  private final MethodVersionDao methodVersionDao;
-  private final RunSetDao runSetDao;
-  private final RunDao runDao;
-  private final GithubMethodDetailsDao githubMethodDetailsDao;
 
-  public MethodService(
-      MethodDao methodDao,
-      MethodVersionDao methodVersionDao,
-      RunSetDao runSetDao,
-      RunDao runDao,
-      GithubMethodDetailsDao githubMethodDetailsDao) {
+  public MethodService(MethodDao methodDao) {
     this.methodDao = methodDao;
-    this.methodVersionDao = methodVersionDao;
-    this.runSetDao = runSetDao;
-    this.runDao = runDao;
-    this.githubMethodDetailsDao = githubMethodDetailsDao;
   }
 
   public void archiveMethod(UUID methodId) {
