5a85e426c19ef60e343b8b7e310cc2853f83a894
Saloni Shah <salonipshah11@gmail.com>
2024-06-18 10:02:36 -0400

    sonarcloud fix

--- service/src/test/java/bio/terra/cbas/models/TestCbasRunStatus.java
+++ service/src/test/java/bio/terra/cbas/models/TestCbasRunStatus.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class TestCbasRunStatus {
+class TestCbasRunStatus {
 
   @ParameterizedTest()
   @CsvSource({
