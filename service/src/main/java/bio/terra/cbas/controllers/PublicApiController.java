42b7ae59b21bbcbf81677daf938cab94f227ccb5
Saloni Shah <salonipshah11@gmail.com>
2024-07-16 13:39:00 -0400

    [WM-2683] Adds /capabilities API (#297)

--- service/src/main/java/bio/terra/cbas/controllers/PublicApiController.java
+++ service/src/main/java/bio/terra/cbas/controllers/PublicApiController.java
@@ -2,8 +2,11 @@ package bio.terra.cbas.controllers;
 
 import bio.terra.cbas.api.PublicApi;
 import bio.terra.cbas.dependencies.common.ScheduledHealthChecker;
+import bio.terra.cbas.model.CapabilitiesResponse;
 import bio.terra.cbas.model.SystemStatus;
 import bio.terra.cbas.model.SystemStatusSystems;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +16,24 @@ import org.springframework.stereotype.Controller;
 public class PublicApiController implements PublicApi {
 
   private final ScheduledHealthChecker scheduledHealthChecker;
+  private final CapabilitiesResponse capabilitiesResponse;
 
   @Autowired
-  public PublicApiController(ScheduledHealthChecker scheduledHealthChecker) {
+  public PublicApiController(
+      ScheduledHealthChecker scheduledHealthChecker, ObjectMapper objectMapper) {
+    // read the "capabilities.json" file from resources and parse it into response object
+    InputStream inputStream = getClass().getResourceAsStream("/capabilities.json");
+    CapabilitiesResponse tempCapabilitiesResponse;
+    try {
+      tempCapabilitiesResponse = objectMapper.readValue(inputStream, CapabilitiesResponse.class);
+      log.info("Capabilities in this CBAS version: {}", tempCapabilitiesResponse);
+    } catch (Exception e) {
+      log.error("Could not read 'capabilities.json' file. Error: {}", e.getMessage(), e);
+      tempCapabilitiesResponse = null;
+    }
+
     this.scheduledHealthChecker = scheduledHealthChecker;
+    this.capabilitiesResponse = tempCapabilitiesResponse;
   }
 
   @Override
@@ -34,5 +51,14 @@ public class PublicApiController implements PublicApi {
                         .addMessagesItem(healthCheck.message())));
 
     return new ResponseEntity<>(result, HttpStatus.OK);
+  }
+
+  @Override
+  public ResponseEntity<CapabilitiesResponse> capabilities() {
+    if (capabilitiesResponse != null) {
+      return new ResponseEntity<>(capabilitiesResponse, HttpStatus.OK);
+    }
+
+    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
---
+++ service/src/test/java/bio/terra/cbas/controllers/TestPublicApiController.java
@@ -0,0 +1,55 @@
+package bio.terra.cbas.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import bio.terra.cbas.model.CapabilitiesResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class TestPublicApiController {
+
+  @Autowired MockMvc mockMvc;
+  @Autowired ObjectMapper objectMapper;
+
+  @Value("classpath:capabilities.json")
+  Resource capabilitiesResource;
+
+  private static final String CAPABILITIES_API = "/capabilities/v1";
+
+  @Test
+  void checkCapabilitiesResponseIsValid() throws Exception {
+    MvcResult mvcResult =
+        mockMvc.perform(get(CAPABILITIES_API)).andExpect(status().isOk()).andReturn();
+
+    CapabilitiesResponse parsedResponse =
+        assertDoesNotThrow(
+            () ->
+                objectMapper.readValue(
+                    mvcResult.getResponse().getContentAsString(), CapabilitiesResponse.class));
+
+    assertThat(parsedResponse).isNotEmpty();
+  }
+
+  @Test
+  void checkCapabilitiesJsonFileIsValid() {
+    assertDoesNotThrow(
+        () ->
+            objectMapper.readValue(
+                capabilitiesResource.getInputStream(), CapabilitiesResponse.class),
+        "Resource file 'capabilities.json' is invalid.");
+  }
+}
